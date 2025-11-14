0ea612aa4e6370e2856206edaea2bd2892807d2c
Simon Thiesen <Simon.Thiesen@online.de>
2025-11-14 12:22:15 +0100

    update

--- src/constants.rs
+++ src/constants.rs
@@ -14,7 +14,7 @@ pub const DRAW_SCALE: f32 = 1.0;
 pub const PIXEL_PER_MODULE: f32 = DRAW_SCALE * 100.;
 pub const LEDS_PER_DIR: usize = 7;
 pub const STEP_SIZE: f32 = 3.;
-pub const COLOR_RADIUS: f32 = 0.2;
+pub const COLOR_RADIUS: f32 = 0.4;
 pub const COLOR_STRENGTH: f32 = 1.0;
 pub const EPSILON: f32 = 1e-4;
 
